6fb19dd6b7919bf036765901d7652d9fa16190d8
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-02-22 12:54:42 -0300

    fet: imports

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
