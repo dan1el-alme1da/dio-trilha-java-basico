16265783c67f71dda1288511a5d58af77d73112f
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-02-22 13:26:00 -0300

    feat: algoritmos de entrada

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -3,6 +3,26 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da Agência!");
+        int numero = scanner.nextInt();
+
+        System.out.println("Informe sua agencia:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, informe seu nome!");
+        String nome = scanner.next();
+
+        System.out.println("Informe o seu saldo atual:");
+        Float saldo = scanner.nextFloat();
+
+
+
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agencia é: "+agencia);
+        System.out.println("Conta: "+numero+", e seu saldo de: "+saldo+" ja esta disponivel para saque");
+       
     }
 }
